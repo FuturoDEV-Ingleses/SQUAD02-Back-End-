e6cd2fb85d9dec4a8ed14b7c7e9bd27753ea8744
Neli33 <118769908+Neli33@users.noreply.github.com>
2023-06-15 19:37:39 -0300

    criação do API

---
+++ src/main/java/com/example/devinadotion/DevinAdotionApplication.java
@@ -0,0 +1,13 @@
+package com.example.devinadotion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevinAdotionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevinAdotionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/devinadotion/DevinAdotionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devinadotion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevinAdotionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
