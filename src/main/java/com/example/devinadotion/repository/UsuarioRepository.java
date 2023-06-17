076abf8422c187e3a7a83e525f0a51b254eb646e
Neli33 <118769908+Neli33@users.noreply.github.com>
2023-06-16 21:02:12 -0300

    criação das entidades

---
+++ src/main/java/com/example/devinadotion/repository/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package com.example.devinadotion.repository;
+
+import com.example.devinadotion.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+}
